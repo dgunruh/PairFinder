18e90836f0a5ad64859f55a0f05a78bed3220dc1
Samantha Tetef <stetef@u.rochester.edu>
2021-11-10 17:53:37 -0800

    fleshed out find_best_pairs function and corresponding test

--- src/best_pairs_finder.py
+++ src/best_pairs_finder.py
@@ -24,48 +24,47 @@ class BestPairsFinder:
                 type == list of tuples
         """
         # Step 1: Check type
-        self._check_data_type()
+        self._check_data_type(particle_positions)
         # Step 2: Enumerate distances
         combinations = self._create_combinations(particle_positions)
         # Step 3: Compute summed distances
         summed_distances = self._get_summed_pair_distance(combinations)
         # Step 4: Return combo that minimizes summed distances
-        best_combo = np.argsort(summed_distances)[0]
-        return combinations[best_combo]
+        return self._choose_best_pair(summed_distances)
 
     def _check_data_type(self):
-        '''
+        """
         Check if object contains tuples of particle coordinates.
-        '''
+        """
         pass
 
     def _create_combinations(self):
-        '''
+        """
         Create all possible combinations of particles.
-        '''
+        """
         pass
 
     def _get_pair_distance(self):
-        '''
+        """
         Calculate the distance between two particles.
-        '''
+        """
         pass
 
     def _get_summed_pair_distance(self):
-        '''
+        """
         Sum the particle pair distances.
-        '''
+        """
         pass
 
     def _sort_pair(self):
-        '''
+        """
         Change pair order and make sure that total distance remains the same.
-        '''
+        """
         pass
 
     def _choose_best_pair(self):
-        '''
+        """
         Choose the best combination of particle pairs from a list of
         combinations and their respective summed distances
-        '''
+        """
         pass
--- test/integration_tests/test_best_pairs_finder.py
+++ test/integration_tests/test_best_pairs_finder.py
@@ -10,58 +10,80 @@ class TestBestPairsFinder(TestCase):
         self.assertTrue(True)
 
     def test__check_data_type(self):
-        '''
+        """
         YIMING
         Check if object contains tuples of particle coordinates.
-        '''
+        """
         pass
 
     def test__create_combinations(self):
-        '''
+        """
         YIMING
         Check whether number of combinations of particles is correct,
         and if all combinations are unique
-        '''
+        """
         pass
 
     def test__get_pair_distance(self):
-        '''
+        """
         LUQING
         Check whether particle distance was calculated correctly.
-        '''
+        """
         pass
 
     def test__get_summed_pair_distance(self):
-        '''
+        """
         LUQING
         Check whether particle distances were summed correctly.
-        '''
+        """
         pass
 
     def test__sort_pair(self):
-        '''
+        """
         YIMING
         Check whether summed distances is invariant to particle pair order.
-        '''
+        """
         pass
 
     def test__choose_best_pair(self):
-        '''
+        """
         Checks whether minimal particle combination was chosen correctly, from
         list of combinations and their respective summed distances
-        '''
+        """
         pass
 
     def test_find_best_pairs(self):
-    	'''
-    	SAM
-
-    	'''
-    	subject = BestPairsFinder()
-    	subject._check_data_type = mock.MagicMock(name='check_dtype',
+        """
+        Test the wrapper function `find_best_pairs` that the user calls.
+        """
+        subject = BestPairsFinder()
+        # setup mocked functions
+        # step 1
+        subject._check_data_type = mock.MagicMock(name='_check_data_type',
                                                   return_value=True)
-    	
-    	pass
+        # step 2
+        combinations = [[(1, 2), (3, 4)],
+                        [(1, 3), (2, 4)],
+                        [(1, 4), (2, 3)]]
+        subject._create_combinations = mock.MagicMock(
+            name='_create_combinations', return_value=combinations)
+        # step 3
+        summed_distances = [2, 4, 4]
+        subject._get_summed_pair_distance = mock.MagicMock(
+            name='_get_summed_pair_distance', return_value=summed_distances)
+        # step 4
+        best_pairing = combinations[0]
+        subject._choose_best_pair = mock.MagicMock(name='_choose_best_pair',
+                                                   return_value=best_pairing)
+        # actually make the call
+        particle_positions = [(1), (2), (3), (4)]
+        result = subject.find_best_pairs(particle_positions)
+        # check all functions were called correctly
+        subject._check_data_type.assert_called_with(particle_positions)
+        subject._create_combinations.assert_called_with(particle_positions)
+        subject._get_summed_pair_distance.assert_called_with(combinations)
+        subject._choose_best_pair.assert_called_with(summed_distances)
+        self.assertEqual(result, best_pairing)
 
     @skip
     def test_find_best_pairs_zero_particles(self):
